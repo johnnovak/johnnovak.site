ad4ebf955a82d9622feb135d38818f7f72c3a56a
John Novak <john@johnnovak.net>
2014-01-21 08:15:41 +1000

    PHOTO: updated templates & photo.js

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -8,13 +8,21 @@
 })(jQuery);
 
 
+// Improved version jQuery find that finds root elements as well
+$.fn.find2 = function(selector) {
+    return this.filter(selector).add(this.find(selector));
+};
+
+
 // NOTE: The following two global params work only with my custom hacked
 // fotorama.js
-var CAPTION_HEIGHT = 65;
+var CAPTION_HEIGHT = 80;
+// iphone: var CAPTION_HEIGHT = 50;
 
 // Value to be subtracted from the stage height calculations (e.g. when
 // height: '100%' is used)
-var HEIGHT_OFFSET = 130;    
+var HEIGHT_OFFSET = 85;
+// iphone: var HEIGHT_OFFSET = 70;
 
 
 /// GLOBAL MODULE START ////////////////////////////////////////////////////////
@@ -50,7 +58,7 @@ function loadFragment(opts) {
 
   $.get(opts.url, function(frag) {
     if (opts.selector) {
-      frag = $(opts.selector, frag);
+      var frag = $(frag).find2(opts.selector);
     }
     frag.imagesLoaded(function() {
       fragment = frag;
@@ -58,72 +66,81 @@ function loadFragment(opts) {
   });
 }
 
+function switchPage(newPage, pathname) {
+  var currPage = site.currentPage;
+  currPage.destroy();
+  newPage.ajaxInit(pathname, currPage.destroyDuration());
+  site.currentPage = newPage;
+}
+
+function switchPageByPathName(pathname) {
+  var newPage = pageFromPathName(pathname);
+  if (newPage.name != site.currentPage.name) {
+    switchPage(newPage, pathname);
+    pushState(pathname);
+  }
+}
+
 function installMenuClickHandler() {
   if (!menuClickHandlerInstalled) {
     $('#header .menu a').each(function(i, link) {
       $(link).on('click', function(event) {
         event.preventDefault();
-        site.currentPage.destroy();
+        switchPageByPathName(link.pathname);
       });
     });
     menuClickHandlerInstalled = true;
   }
 };
 
+function splitPathName(pathname) {
+  var p = pathname.split('/');
+  // Remove initial and trailing forward slashes, if present.
+  if (!p[0]) {
+    p.shift();
+  }
+  if (!p[p.length - 1]) {
+    p.pop();
+  }
+  return p;
+}
+
+function pageNameFromPathName(pathname) {
+  var path = splitPathName(pathname); 
+  if (path.length == 2 && path[1] == 'about') {
+    return 'about';
+  } else if (path.length == 1 || path.length == 2) {
+    return 'albums';
+  } else if (path.length == 3) {
+    return 'photo';
+  }
+}
+
+function pageFromPathName(pathname) {
+  return pageMappings[pageNameFromPathName(pathname)];
+}
+
 var historyPushed = false;
 
 function pushState(url) {
+  console.log('*** pushState:', url);
   history.pushState(null, null, url);
   historyPushed = true;
 }
 
 function installPopStateHandler() {
-  function splitPathName(pathname) {
-    var p = pathname.split('/');
-    // Remove initial and trailing forward slashes, if present.
-    if (!p[0]) {
-      p.shift();
-    }
-    if (!p[p.length - 1]) {
-      p.pop();
-    }
-    return p;
-  }
-
   window.addEventListener('popstate', function(e) {
     if (historyPushed) {
       var currPage = site.currentPage;
-      var path = splitPathName(location.pathname); 
-      var page;
+      var pathname = location.pathname;
+      var newPage = pageFromPathName(pathname);
 
-      // about
-      if (path.length == 1 && path[0] == 'about') {
-        currPage.destroy();
-        // TODO load 'about'
-        console.log('load(about)');
-
-      // albums
-      } else if (path.length == 1 || path.length == 2) {
-        if (currPage.name == 'albums') {
-          currPage.changeCategory(location.pathname);
-        } else {
-          currPage.destroy();
-          // TODO load 'albums'
-          console.log('load(albums)');
+      if (currPage.name == newPage.name) {
+        if (currPage.popStateHandler) {
+          currPage.popStateHandler();
         }
-
-      // photo
-      } else if (path.length == 3) {
-        if (currPage.name == 'photo') {
-          var hash = location.hash.replace(/^#/, '');
-          if (hash) {
-            currPage.getFotoramaApi().show(hash);
-          }
-        } else {
-          currPage.destroy();
-          // TODO load 'photo'
-          console.log('load(photo)');
-        }
+      } else {
+        switchPage(newPage, pathname);
       }
     }
   });
@@ -155,7 +172,6 @@ var photo = function() {
 
       var heading = $(h2[i]);
       var title = heading.text();
-      console.log(title);
       heading.remove();
 
       var image = $(img[i]);
@@ -233,7 +249,6 @@ var photo = function() {
 
     function pushCurrentPhoto(e, fotorama) {
       var hash = fotorama.activeFrame.id || fotorama.activeIndex;
-      console.log('fotorama:show', hash);
       if (location.hash.replace(/^#/, '') != hash) {
         pushState('#' + hash);
       }
@@ -249,7 +264,9 @@ var photo = function() {
   }
 
   function init() {
-    installMenuClickHandler()
+    if (hasHistoryApi) {
+      installMenuClickHandler()
+    }
     fotoramaize();
     createNavigation();
     createFotorama();
@@ -257,15 +274,44 @@ var photo = function() {
     installImageCounterAndCaptionHandlers();
   }
 
+  function ajaxInit(url, delay) {
+    loadFragment({
+      url: url,
+      selector: '#content',
+      minDelay: delay,
+      onLoad: function(fragment) {
+        // TODO refactor into method
+        $('#content').html(fragment);
+        init();
+      }
+    });
+  }
+
+  var fadeOutDuration = 300;
+
   function destroy() {
-    console.log('photo.destroy');
+    $('#fotorama').fadeTo(fadeOutDuration, 0);
+    $('#nav').fadeTo(fadeOutDuration, 0);
+  }
+
+  function destroyDuration() {
+    return fadeOutDuration;
+  }
+
+  function popStateHandler() {
+    var hash = location.hash.replace(/^#/, '');
+    if (hash) {
+      fotoramaApi.show(hash);
+    }
   }
 
   return {
     name: 'photo',
     init: init,
+    ajaxInit: ajaxInit,
     destroy: destroy,
-    getFotoramaApi: function() { return fotoramaApi; }
+    destroyDuration: destroyDuration,
+    popStateHandler: popStateHandler
   };
 }();
 
@@ -274,37 +320,28 @@ var photo = function() {
 var albums = function() {
   var categories;
   var loading = false;
-  var fadeInDone = false;
 
   function fadeInAlbums(initialDelay) {
     var fadeInDelay = 120;
     var fadeInDuration = 400;
     initialDelay |= 0;
 
-    fadeInDone = false;
     $('.album').each(function(i, album) {
       $(album).delay(initialDelay + i * fadeInDelay) .fadeTo(fadeInDuration, 1);
     });
-    var duration = initialDelay + ( $('.album').length - 1)
-                                * fadeInDelay + fadeInDuration;
-    setTimeout(function() { fadeInDone = true }, duration);
   }
 
-  var fadeOutDelay = 120;
-  var fadeOutDuration = 200;
-  var fadeOutDone = false;
+  var fadeOutAlbumsDelay = 120;
+  var fadeOutAlbumDuration = 200;
 
   function fadeOutAlbums() {
-    fadeOutDone = false;
     $('.album').each(function(i, album) {
-      $(album).delay(i * fadeOutDelay).fadeTo(fadeOutDuration, 0);
+      $(album).delay(i * fadeOutAlbumsDelay).fadeTo(fadeOutAlbumDuration, 0);
     });
-    var duration = fadeOutAlbumsDuration();
-    setTimeout(function() { fadeOutDone = true }, duration);
   }
 
   function fadeOutAlbumsDuration() {
-    return ($('.album').length - 1) * fadeOutDelay + fadeOutDuration;
+    return ($('.album').length - 1) * fadeOutAlbumsDelay + fadeOutAlbumDuration;
   }
 
   function categoryByHref(url) {
@@ -321,19 +358,18 @@ var albums = function() {
     categories.removeClass('sel');
     categoryByHref(url).addClass('sel');
     fadeOutAlbums();
-    loading = true;
     loadCategory(url);
   }
 
   function loadCategory(url) {
     loadFragment({
       url: url,
-      selector: '.thumbs > *',
+      selector: '.thumbs',
       minDelay: fadeOutAlbumsDuration(),
       onLoad: function(fragment) {
-        loading = false;
-        $('.thumbs').html(fragment);
+        $('.thumbs').html(fragment.children());
         fadeInAlbums();
+        installAlbumClickHandler();
       }
     });
   }
@@ -344,38 +380,87 @@ var albums = function() {
     });
   }
 
+  var fadeOutCategoriesInitialDelay = 50;
+  var fadeOutCategoriesDelay = 120;
+  var fadeOutCategoryDuration = 200;
+
+  function fadeOutCategories() {
+    categories.each(function(i, album) {
+      $(album).delay(fadeOutCategoriesInitialDelay + i * fadeOutCategoriesDelay).fadeTo(fadeOutCategoryDuration, 0);
+    });
+  }
+
+  function fadeOutCategoriesDuration() {
+    return fadeOutCategoriesInitialDelay + ($('.album').length - 1) * fadeOutCategoriesDelay + fadeOutCategoryDuration;
+  }
+
   function installCategoryClickHandler() {
     categories.find('a').each(function(i, link) {
       $(link).on('click', function(event) {
         event.preventDefault();
-        if (!loading && fadeInDone) {
-          pushState(link.pathname);
-          changeCategory(link.pathname);
-        }
+        changeCategory(link.pathname);
+        pushState(link.pathname);
+      });
+    });
+  }
+
+  function installAlbumClickHandler() {
+    $('.thumbs .album').each(function(i, album) {
+      $('a', album).each(function(i, link) {
+        $(link).on('click', function(event) {
+          event.preventDefault();
+          switchPageByPathName(link.pathname);
+        });
       });
     });
   }
 
   function init() {
-    installMenuClickHandler()
     categories = $('.categories li');
+    // refactor into globalInit
     if (hasHistoryApi) {
-      installPopStateHandler();
+      installMenuClickHandler()
       installCategoryClickHandler();
+      installAlbumClickHandler();
     }
     fadeInAlbums(250);
     fadeInCategories();
   }
 
+  function ajaxInit(url, delay) {
+    loadFragment({
+      url: url,
+      selector: '#content',
+      minDelay: delay,
+      onLoad: function(fragment) {
+        // TODO refactor into method
+        $('#content').html(fragment.children());
+        init();
+      }
+    });
+  }
+
   function destroy() {
-    console.log('albums.destroy');
+    fadeOutCategories()
+    fadeOutAlbums();
+  }
+
+  function destroyDuration() {
+    return Math.max(fadeOutCategoriesDuration(), fadeOutAlbumsDuration());
+  }
+
+  function popStateHandler() {
+    changeCategory(location.pathname);
   }
 
   return {
     name: 'albums',
     init: init,
+    ajaxInit: ajaxInit,
     destroy: destroy,
-    changeCategory: changeCategory
+    destroyDuration: destroyDuration,
+    changeCategory: changeCategory,
+    popStateHandler: popStateHandler
   }
 }();
 
@@ -387,21 +472,64 @@ var about = function() {
     $('#about .text').delay(400).fadeTo(600, 1);
   }
 
+  var imgFadeOutDelay = 0;
+  var imgFadeOutDuration = 150;
+  var textFadeOutDelay = 60;
+  var textFadeOutDuration = 250;
+
+  function fadeOut() {
+    $('#about img').delay(imgFadeOutDelay).fadeTo(imgFadeOutDuration, 0);
+    $('#about .text').delay(textFadeOutDelay).fadeTo(textFadeOutDuration, 0);
+  }
+
+  function fadeOutDuration() {
+    return Math.max(imgFadeOutDelay + imgFadeOutDuration,
+                    textFadeOutDelay + textFadeOutDuration);
+  }
+
   function init() {
-    installMenuClickHandler()
+    if (hasHistoryApi) {
+      installMenuClickHandler()
+    }
     fadeIn();
   }
 
+  function ajaxInit(url, delay) {
+    loadFragment({
+      url: url,
+      selector: '#content',
+      minDelay: delay,
+      onLoad: function(fragment) {
+        // TODO refactor into method
+        $('#content').html(fragment.children());
+        init();
+      }
+    });
+  }
+
   function destroy() {
-    console.log('about.destroy');
+    console.log("destroy about");
+    fadeOut();
+  }
+
+  function destroyDuration() {
+    return fadeOutDuration();
   }
 
   return {
     name: 'about',
     init: init,
-    destroy: destroy
+    ajaxInit: ajaxInit,
+    destroy: destroy,
+    destroyDuration: destroyDuration
   }
 }();
+
+var pageMappings = {
+  'about': about,
+  'albums': albums,
+  'photo': photo
+}
 
 /// GLOBAL MODULE EXPORTS //////////////////////////////////////////////////////
 
